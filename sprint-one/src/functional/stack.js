1645b430cf21d85350fb8e08337bf1ca72d8823a
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-04 11:33:54 -0800

    "Initial attempt at each function; failing some size and pop tests"

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,18 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size] = value;
+    someInstance.size();
   };
 
   someInstance.pop = function() {
+    let element = storage[someInstance.size]
+    someInstance.size();
+    return element;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
