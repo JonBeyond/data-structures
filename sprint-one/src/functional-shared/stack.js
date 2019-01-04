3c370ea1c1732fcd18241d25fa0c4b9e36d4459a
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-04 15:26:25 -0800

    "Started but was unable to determine how to utilize functional-shared without using `this`"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	var anInstance = {};
+	anInstance.size = 0;
+	anInstance.storage = {}; // store the storage as a property for access
+	anInstance = Object.assign(anInstance, stackMethods); //this will add a reference to stackMethods
+	return anInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	pop: function (){
+		if (this.size > 0){
+			let element = this.storage[this.size];
+			delete this.storage[this.size];
+			return element;
+		}
+	},
+	push: function (val){
+		//add to the end
+		this.storage[this.size] = val;
+		this.size++;
+	},
+	size: function(){
+		return this.size; //adjust size
+	}
 
-
+};
