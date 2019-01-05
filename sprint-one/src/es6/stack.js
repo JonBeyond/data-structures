b7f408e7f6e9fd3afc1d4217f7c823b344966f4d
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-04 19:59:31 -0800

    "COmpleted stack ES6.  Easiest!"

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {};
+  	this.currentSize = 0;
   }
+  pop(){
+	if (this.currentSize > 0){
+		this.currentSize--;
+		let element = this.storage[this.currentSize];
+		return element;
+	}
+  }
+  push(val){
+  	this.storage[this.currentSize] = val;
+	this.currentSize++;
+  }
+  size(){
+  	return this.currentSize;
+  }
+
 
 }
