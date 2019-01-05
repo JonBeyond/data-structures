1fb98b5f8006d3060db9b0b89d995bc7c24d7cee
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-04 18:05:25 -0800

    "Completed stack.js prototypal"

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,18 +1,28 @@
 var Stack = function() {
-	var anInstance = Object.create(Stack.prototype);
+	var anInstance = Object.create(stackMethods);
 	anInstance.storage = {};
 	anInstance.currentSize = 0;
 	return anInstance;
-	//storage
-	//size
-	//
-
 };
 
 var stackMethods = {
-	//pop
-	//push
-	//size
+
 };
 
+stackMethods.pop = function() {
+	if (this.currentSize > 0){
+		this.currentSize--;
+		let element = this.storage[this.currentSize];
+		delete this.storage[this.currentSize];
+		return element;
+	}
+};
 
+stackMethods.push = function(val) {
+	this.storage[this.currentSize] = val;
+	this.currentSize++;
+};
+
+stackMethods.size = function() {
+	return this.currentSize;
+};
