6f4358df69fdf7ac875ab5fcbf417246edadb966
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-04 18:21:17 -0800

    "Completed stack pseudoclassical very quickly"

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	this.storage = {};
+	this.currentSize = 0;
 };
 
+Stack.prototype.pop = function(){
+	if (this.currentSize > 0){
+		this.currentSize--;
+		let element = this.storage[this.currentSize];
+		return element;
+	}
+};
+Stack.prototype.push = function(val){
+	this.storage[this.currentSize] = val;
+	this.currentSize++;
+};
+Stack.prototype.size = function(){
+	return this.currentSize;
+}
 
+var hello = new Stack();
