00ec80e754b8ec872c55d6210116ce142aa4929d
JonBeyond <jonathan.mitzman@gmail.com>
2019-01-07 10:54:24 -0800

    "Completed tree"

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,40 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  
+  Object.assign(newTree, treeMethods);
+  
   return newTree;
+/*
+newTree.addChild = function(value) {
+  let childTree = Tree(value); //why does let not work?
+  this.children.push(childTree);
+};
+*/
+
+
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var childTree = Tree(value); //why does let not work?
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else {
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
-
 
 
 /*
