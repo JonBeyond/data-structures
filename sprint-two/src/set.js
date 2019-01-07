f9c35fb741409736564f9f958a3c8fe344792629
JonBeyond <jonathan.mitzman@gmail.com>
2019-01-07 11:20:47 -0800

    refactor

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,12 +1,17 @@
 var Set = function() {
+  // var set = {}; 
+  // Object.assign(set, setPrototype)
+  // the below line of code is the same as the above two lines
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+
+  set.storage = {}; // was set._storage
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
@@ -18,3 +23,7 @@ setPrototype.remove = function(item) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+let aSet = Set();
+aSet.add('hello');
+console.log(JSON.stringify(aSet));
