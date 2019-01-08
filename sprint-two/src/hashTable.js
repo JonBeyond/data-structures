d99c4fcbaee1d39bcdaf67abb5c8d796f9abe6b0
JonBeyond <jonathan.mitzman@gmail.com>
2019-01-07 20:34:11 -0800

    hash table insert, retrieve, and delete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,28 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[index] = [];
+  this._storage[index].push([k, v]);
+  //create a tuple and add it to an index in the 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (let element of this._storage[index]) {
+    if (element[0] === k) {
+      return element[1]
+  }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  debugger;
+  for (let i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      this._storage[index].splice(i, 1);
+    }
+  }
 };
 
 
