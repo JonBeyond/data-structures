496155071af4d2f081b1a2bc32d6bd2b128a958e
JonBeyond <jonathan.mitzman@gmail.com>
2019-01-08 17:02:04 -0800

    binary search tree insert and contains

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,13 +1,9 @@
-let someTree = new BinarySearchTree(5);
-someTree.insert(4);
-debugger;
-
 var BinarySearchTree = function(value) {
-  var binaryTree = Object.create(binaryTreeMethods)
-  binaryTree.left = null;
-  binaryTree.right = null;
-  binaryTree.value = value;
-  return binaryTree;
+  var treeRoot = Object.create(binaryTreeMethods)
+  treeRoot.left = null;
+  treeRoot.right = null;
+  treeRoot.value = value;
+  return treeRoot;
 };
 
 var binaryTreeMethods = {};
@@ -30,32 +26,37 @@ binaryTreeMethods.insert = function(value) {
       }
     }
   }
-  deeper(newNode, this.binaryTree);
+  deeper(newNode, this);
 }
-    /*var deeper = function(newNode, current) {
-      if (newNode.value > current.value) {  //go to the right
-        if (newNode.value < current.right.value) {
-          let former = current.right;  //save current.right
-          current.right = newNode;  // insert newNode.value at current.right
-          newNode.right = former;  //add former to the right of newNode.value
-        } else {
-          deeper(newNode, current.right);
-        }
-      } else { //go to the left
-        if (newNode.value > current.left.value) {
-          let former = current.left;  //same process as earlier
-          current.left = newNode;
-          newNode.left = former;
-        } else {
-          deeper(newNode, current.left);
-        }
-    }
-    deeper(newNode, this.binaryTree);
-  }
-}*/
 
-binaryTreeMethods.contains = function(value) {
+
+binaryTreeMethods.contains = function(target) {
+  var checkDeeper = function (currentNode) {
+    if (target > currentNode.value){
+      //go right
+      if (currentNode.right === null){
+        return false;
+      } else if (currentNode.right.value === target){
+        return true;
+      } else {
+        return checkDeeper(currentNode.right);
+      }
+    } else {
+      //go left
+      if (currentNode.left === null) {
+        return false;
+      } else if (currentNode.left.value === target) {
+          return true;
+      } else {
+          return checkDeeper(currentNode.left);
+      }
+    }
+    
   }
+  // debugger;
+  return checkDeeper(this); 
+  
+}
   
 binaryTreeMethods.depthFirstLog = function(cb) {
 
@@ -76,4 +77,39 @@ binaryTreeMethods.depthFirstLog = function(cb) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+var someTree = BinarySearchTree(5);
+someTree.insert(2);
+someTree.insert(3);
+someTree.insert(7);
+someTree.insert(6);
+console.log(someTree);
 
+
+//debugger;
+
+
+
+    /* the below implementation of "insert" creates an incorrect binary tree
+
+
+      var deeper = function(newNode, current) {
+      if (newNode.value > current.value) {  //go to the right
+        if (newNode.value < current.right.value) {
+          let former = current.right;  //save current.right
+          current.right = newNode;  // insert newNode.value at current.right
+          newNode.right = former;  //add former to the right of newNode.value
+        } else {
+          deeper(newNode, current.right);
+        }
+      } else { //go to the left
+        if (newNode.value > current.left.value) {
+          let former = current.left;  //same process as earlier
+          current.left = newNode;
+          newNode.left = former;
+        } else {
+          deeper(newNode, current.left);
+        }
+    }
+    deeper(newNode, this.binaryTree);
+  }
+}*/
